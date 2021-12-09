953a912510fa92c8212d93cbd6b8ea1ce281201f
Ze Zhou <zezhou96@gmail.com>
2021-12-09 12:42:37 -0700

    Updated index page

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,7 @@
-import { useEffect, useState } from 'react'
-import {collection, getDocs} from 'firebase/firestore'
-import {db} from 'libs/firebase'
+import Link from 'next/link'
 
-function UserProfile({age, fullName, address, ...props}){
-    return(
-        <ul>
-            <li>{fullName}</li>
-            <li>{age}</li>
-            <li>{address.city}</li>
-        </ul>
-    )
-}
-
-function Index(props){
-    // Read a single Doc from a collection
-    const [users, setUsers] = useState([])
-
-    useEffect(()=>{
-        async function getFirebaseDoc(){
-            const ref = collection(db, 'users')
-            const userSnapshot = await getDocs(ref)
-
-            userSnapshot.forEach(doc=>{
-                // object and array non mutating methods of state...
-                setUsers(doc.data())
-            })
-        }
-        getFirebaseDoc()
-    }, [])
-    
-    if (users) {
-        return <div>Getting a Group of Docs</div>
-    }else{
-        return <div>Getting a Group of Docs</div>
-    }
+function Index(props) {
+return (<Link href="/login"><a>Login</a></Link>)
 }
 
 export default Index
